ee32eb54eac0fb419226613c540e15f32d55bfe1
Thien Tran <gau.nernst@yahoo.com.sg>
2023-11-14 07:57:00 +0800

    refactor hittable

--- include/raytracing.h
+++ include/raytracing.h
@@ -1,90 +1,10 @@
 #ifndef RAYTRACING_H
 #define RAYTRACING_H
 
+#include "hittable.h"
 #include "material.h"
-#include "utils.h"
 #include "vec3.h"
-#include <stdbool.h>
-#include <stdlib.h>
-
-typedef struct Ray {
-  Vec3 origin;
-  Vec3 direction;
-} Ray;
-
-Vec3 ray_at(const Ray *ray, float t);
-
-typedef enum HittableType {
-  HITTABLE_LIST,
-  SPHERE,
-  QUAD,
-  TRANSLATE,
-  ROTATE_Y,
-} HittableType;
-
-typedef struct Hittable {
-  HittableType type;
-  void *ptr;
-} Hittable;
-
-#define hittable(ptr)                                                                                                  \
-  (Hittable) {                                                                                                         \
-    _Generic((ptr),                                                                                                    \
-        HittableList *: HITTABLE_LIST,                                                                                 \
-        Sphere *: SPHERE,                                                                                              \
-        Quad *: QUAD,                                                                                                  \
-        Translate *: TRANSLATE,                                                                                        \
-        RotateY *: ROTATE_Y),                                                                                          \
-        ptr                                                                                                            \
-  }
-
-define_list_header(Hittable);
-
-bool Hittable_hit(Hittable obj, const Ray *ray, float t_min, float t_max, HitRecord *hit_record);
-bool HittableList_hit(const HittableList *list, const Ray *ray, float t_min, float t_max, HitRecord *hit_record);
-
-typedef struct Sphere {
-  Vec3 center;
-  float radius;
-  Material material;
-} Sphere;
-
-Sphere *Sphere_new(Vec3 center, float radius, Material material);
-
-typedef struct Quad {
-  Vec3 Q;
-  Vec3 u;
-  Vec3 v;
-  Vec3 normal;
-  float D;
-  Vec3 w;
-  Material material;
-} Quad;
-
-void Quad_init(Quad *quad, Vec3 Q, Vec3 u, Vec3 v, Material mat);
-Quad *Quad_new(Vec3 Q, Vec3 u, Vec3 v, Material mat);
-
-HittableList *Box_new(Vec3 a, Vec3 b, Material mat);
-
-typedef struct Translate {
-  Hittable object;
-  Vec3 offset;
-} Translate;
-
-Translate *Translate_new(Hittable object, Vec3 offset);
-
-typedef struct RotateY {
-  Hittable object;
-  float sin_theta;
-  float cos_theta;
-} RotateY;
-
-void RotateY_init(RotateY *rotate_y, Hittable object, float angle);
-RotateY *RotateY_new(Hittable object, float angle);
-
-// typedef struct AABB {
-//   float x[3][2];
-// } AABB;
+#include <stddef.h>
 
 typedef struct World {
   HittableList objects;
