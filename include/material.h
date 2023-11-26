e4540ddd4e456d168bda77eb24718ee0392b38a9
Thien Tran <gau.nernst@yahoo.com.sg>
2023-11-26 20:12:55 +0800

    use one struct for all Material

--- include/material.h
+++ include/material.h
@@ -12,6 +12,11 @@ typedef struct HitRecord HitRecord;
 struct Material {
   bool (*scatter)(HitRecord *rec, Vec3 incident, PCG32 *rng, Vec3 *scattered, Vec3 *color);
   Vec3 (*emit)(HitRecord *rec);
+  Texture *albedo;
+  union {
+    float fuzz; // for metal
+    float eta;  // for dielectric
+  };
 };
 
 struct HitRecord {
@@ -24,46 +29,22 @@ struct HitRecord {
   bool front_face;
 };
 
-typedef struct SurfaceNormal {
-  Material mat;
-} SurfaceNormal;
-void SurfaceNormal_init(SurfaceNormal *self);
+void SurfaceNormal_init(Material *self);
 Material *SurfaceNormal_new();
 
-typedef struct Lambertian {
-  Material mat;
-  Texture *albedo;
-} Lambertian;
-void Lambertian_init(Lambertian *self, Texture *albedo);
+void Lambertian_init(Material *self, Texture *albedo);
 Material *Lambertian_new(Texture *albedo);
 
-typedef struct Metal {
-  Material mat;
-  Texture *albedo;
-  float fuzz;
-} Metal;
-void Metal_init(Metal *self, Texture *albedo, float fuzz);
+void Metal_init(Material *self, Texture *albedo, float fuzz);
 Material *Metal_new(Texture *albedo, float fuzz);
 
-typedef struct Dielectric {
-  Material mat;
-  float eta;
-} Dielectric;
-void Dielectric_init(Dielectric *self, float eta);
+void Dielectric_init(Material *self, float eta);
 Material *Dielectric_new(float eta);
 
-typedef struct DiffuseLight {
-  Material mat;
-  Texture *albedo;
-} DiffuseLight;
-void DiffuseLight_init(DiffuseLight *self, Texture *albedo);
+void DiffuseLight_init(Material *self, Texture *albedo);
 Material *DiffuseLight_new(Texture *albedo);
 
-typedef struct Isotropic {
-  Material mat;
-  Texture *albedo;
-} Isotropic;
-void Isotropic_init(Isotropic *self, Texture *albedo);
+void Isotropic_init(Material *self, Texture *albedo);
 Material *Isotropic_new(Texture *albedo);
 
 #endif // MATERIAL_H
